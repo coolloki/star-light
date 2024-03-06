4c862540173a182e6ba396350346a08c3a18fa3b
coolloki <cooool.loki@gmail.com>
2024-03-06 17:47:33 -0600

    Added Category and Team models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.functions import Lower
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=250, verbose_name="Category's name")
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = [Lower("title")]
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Team's name")
+    categories = models.ManyToManyField(Category, limit_choices_to={'is_active': True})
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Teams"
